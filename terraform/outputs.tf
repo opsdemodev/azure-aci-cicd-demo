6ab0eebbe6feb87acc16ea2d5523ccf49a11f55d
opsdemodev <opsdemodev@gmail.com>
2026-09-17 11:18:42 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,16 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.main.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.main.login_server
+}
+
+output "acr_admin_username" {
+  value     = azurerm_container_registry.main.admin_username
+  sensitive = true
+}
